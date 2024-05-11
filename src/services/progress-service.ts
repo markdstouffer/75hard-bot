80b32b5979abfaff878c2ba005e8619a07a51035
himarks <markdstouffer@gmail.com>
2024-05-11 16:57:55 -0400

    view progress

--- src/services/progress-service.ts
+++ src/services/progress-service.ts
@@ -1,5 +1,7 @@
 import {supabase} from "@utils/supabase";
-import {Function} from "@internalTypes/database";
+import {CompleteProgress, Function, Progress, Table} from "@internalTypes/database";
+import {User} from "discord.js";
+import {getThisSunday} from "@utils/time";
 
 export class ProgressService {
 
@@ -10,6 +12,27 @@ export class ProgressService {
 
         if (error)
             throw error;
+    }
+
+    public static getForUserThisWeek = async (user: User): Promise<CompleteProgress[]> => {
+        const {id} = user;
+
+        const thisSunday = getThisSunday();
+
+        const {data, error} = await supabase
+            .from(Table.Progress)
+            .select(`
+                *,
+                ${Table.Goals}!inner(title, frequency),
+                ${Table.Users}!inner(discord_id)
+            `)
+            .eq("users.discord_id", id)
+            .eq("week_start", thisSunday);
+
+        if (error)
+            throw error;
+
+        return data;
     }
 
 }
